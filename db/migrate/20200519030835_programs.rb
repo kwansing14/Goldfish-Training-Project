af754ec32add7da5ebba28cea7d05a7b20ab879a
lekhweemeng <lek.hweemeng@live.com>
2020-05-19 14:24:45 +0800

    updated name into the program table

--- db/migrate/20200519030835_programs.rb
+++ db/migrate/20200519030835_programs.rb
@@ -1,6 +1,7 @@
 class Programs < ActiveRecord::Migration[5.2]
   def change
     create_table :programs do |t|
+      t.string :name
       t.string :duration
       t.integer :cost
       t.integer :contact_no
