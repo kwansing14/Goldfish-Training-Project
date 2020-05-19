5de58b747d7a7883c1861be6a950aaedf7c6f040
lekhweemeng <lek.hweemeng@live.com>
2020-05-19 11:53:08 +0800

    added in controllers and some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# soccer = Program.create(
+#   :name => 'Weird Al Yankovich', :photo_url => 'http://i.huffpost.com/gen/1952378/images/o-WEIRD-AL-facebook.jpg', :nationality => 'American'
+# )
