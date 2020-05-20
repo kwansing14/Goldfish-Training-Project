1d842305cabd816d0c0bb4f030ac6b1082330bdf
lekhweemeng <lek.hweemeng@live.com>
2020-05-20 15:04:46 +0800

    completed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-soccer = Program.create(
-  :duration => '2 days', :cost => '100', :contact_no => '12345678'
+
+
+soccer = Program.create(:name => 'Soccer',
+  :duration => '2 days', :cost => 100, :contact_no => '12345678'
 )
+
+basketball = Program.create(:name => 'Basketball',
+  :duration => '3 days', :cost => 100, :contact_no => '12345678'
+)
+
+rubgy = Program.create(:name => 'Rugby',
+  :duration => '5 days', :cost => 100, :contact_no => '12345678'
+)
+
+tricks = Program.create(:name => 'Tricks',
+  :duration => '14 days', :cost => 500, :contact_no => '12345678'
+)
+
+enrolsoccer = Programs_User.create(:user_id => 1, :program_id => 1)
+
+enrolbball = Programs_User.create(:user_id => 1, :program_id => 2)
+
+enroltricks = Programs_User.create(:user_id => 1, :program_id => 2)
+
+enroltricks2 = Programs_User.create(:user_id => 2, :program_id => 2)
