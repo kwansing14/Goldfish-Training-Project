e929a6f1bf541d0973b98e567ed1fabc27bbb2f2
lekhweemeng <lek.hweemeng@live.com>
2020-05-23 23:47:30 +0800

    updated comments controller -> changed ids items

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,23 @@
 class CommentsController < ApplicationController
   def show
-    @fishtagram = Fishtagram.find(params[:pin_id])
+    @fishtagram = Fishtagram.find(params[:fishtagram_id])
   end
 
   def index
   end
 
   def new
-    @fishtagram = Fishtagram.find(params[:pin_id])
+    @fishtagram = Fishtagram.find(params[:fishtagram_id])
   end
 
   def create
-    @fishtagram = Fishtagram.find(params[:pin_id])
+    @fishtagram = Fishtagram.find(params[:fishtagram_id])
     @comment = Comment.new(comment_params)
     @comment.user = current_user
     @comment.Fishtagram = @fishtagram
 
     if @comment.save
-      redirect_to pin_path(@fishtagram)
+      redirect_to fishtagram_path(@fishtagram)
     else
       byebug
       render 'new'
@@ -25,19 +25,19 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = Comment.find(params[:pin_id])
+    @comment = Comment.find(params[:fishtagram_id])
   end
 
   def update
     @fishtagram = Fishtagram.find(params[:id])
-    @comment = Comment.find(params[:pin_id])
+    @comment = Comment.find(params[:fishtagram_id])
     @comment.update(comment_params)
 
     redirect_to @fishtagram
   end
 
   def destroy
-    @comment = Comment.find(params[:pin_id])
+    @comment = Comment.find(params[:fishtagram_id])
     @comment.destroy
     redirect_to root_path
   end
@@ -45,6 +45,5 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:body)
->>>>>>> 762e8a7a25dcf1b52a413875e77deeb633655aa8
   end
 end
