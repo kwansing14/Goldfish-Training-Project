38d395eac85a86122e99daa4cc3a1fb1402e6de9
lekhweemeng <lek.hweemeng@live.com>
2020-05-23 22:48:16 +0800

    updated fishtagrams wordings

--- app/controllers/fishtagrams_controller.rb
+++ app/controllers/fishtagrams_controller.rb
@@ -1,25 +1,25 @@
 class FishtagramsController < ApplicationController
   def show
-    @fishstagram = Fishstagram.find(params[:id])
+    @fishstagram = Fishtagram.find(params[:id])
     @comments = Comment.where(pin_id: (params[:id]))
   end
 
   def edit
-    @fishstagram = Fishstagram.find(params[:id])
+    @fishstagram = Fishtagram.find(params[:id])
   end
 
   def index
-    @fishtagrams = Fishstagram.all
+    @fishtagrams = Fishtagram.all
   end
 
   def update
-    @fishstagram = Fishstagram.find(params[:id])
+    @fishstagram = Fishtagram.find(params[:id])
     @fishstagram.update(pin_params)
     redirect_to @fishstagram
   end
 
   def destroy
-    @fishstagram = Fishstagram.find(params[:id])
+    @fishstagram = Fishtagram.find(params[:id])
     @fishstagram.destroy
 
     redirect_to root_path
