aca6303cfabcdf4801a13079be012b023c003050
travisenquiry <travisenquiry@gmail.com>
2020-05-26 15:11:20 +0800

    commit for merge to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
       # For APIs, you may want to use :null_session instead.
       protect_from_forgery with: :exception
 
+  before_action :set_constants
+    def set_constants
+      if current_user
+        @programs_user = Programs_user.where(user_id: current_user.id)
+        @enrolled = User.joins(:programs).where(user_id: current_user.id)
+      end
+    end
+
 before_action :configure_permitted_parameters, if: :devise_controller?
 
       protected
